90b456e69564b6fb0cdaa1bc5d85cc3c663260ea
Dip-Baidya <dipbaidya.cse@gmail.com>
2021-09-18 03:45:14 +0600

    right know api didn't give me data but why i don't know

---
+++ js/app.js
@@ -0,0 +1,22 @@
+document.getElementById('search').addEventListener('click', function(){
+    getWeather();
+})
+
+function getWeather() {
+    const apiKey = `89284f2573b239b4fc5e51d5493f7980`;
+    const inputCity = document.getElementById('Input-city').value;
+
+    fetch(`api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${apiKey}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const statusIcon = data.weather[0].icon;
+            document.getElementById("status-icon").src = statusIcon;
+            const cityName = data.name;
+            document.getElementById("city-name").innerText = cityName;
+            const temperature = data.main.temp;
+            document.getElementById("temperature").innerText = temperature;
+            const status = data.weather[0].main;
+            document.getElementById("status").innerText = status;
+        })
+        .catch(error => console.log(error));
+}
